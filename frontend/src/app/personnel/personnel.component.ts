d934110dea03bd9ca38b559084b0228d0cedddfc
yaafer86 <yaafer86@gmail.com>
2024-12-31 10:42:13 +0000

    add angularjs with ts component

---
+++ frontend/src/app/personnel/personnel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnel',
+  imports: [],
+  templateUrl: './personnel.component.html',
+  styleUrl: './personnel.component.css'
+})
+export class PersonnelComponent {
+
+}
---
+++ frontend/src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle',
+  imports: [],
+  templateUrl: './vehicle.component.html',
+  styleUrl: './vehicle.component.css'
+})
+export class VehicleComponent {
+
+}
